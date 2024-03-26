37adc225d8930afe6656cd15a0357a9e99dc9401
Caue Gomes de Oliveira <cauego96@hotmail.com>
2024-03-25 23:02:40 -0300

    AI Autocomplete

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -8,11 +8,18 @@ import { useDebounce } from '@/lib/useDebounce'
 import { useMutation } from '@tanstack/react-query'
 import { NoteType } from '@/lib/db/schema'
 import axios from 'axios'
+import Text from '@tiptap/extension-text'
+import { useCompletion } from 'ai/react'
 
 type Props = {note: NoteType};
 
 const TipTapEditor = ({note}: Props) => {
-    const [editorState, setEditorState] = React.useState(note.editorState || "");
+    const [editorState, setEditorState] = React.useState(
+        note.editorState || '<h1>${note.name}</h1>'
+    );
+    const {complete, completion} = useCompletion({
+        api: '/api/completion',
+    });
     const saveNote = useMutation({
         mutationFn: async () => {
             const response = await axios.post('/api/saveNote',{
@@ -21,15 +28,39 @@ const TipTapEditor = ({note}: Props) => {
             });
             return response.data;
         }
-    })
+    });
+    const customText  = Text.extend({
+        addKeyboardShortcuts() {
+            return {
+                "Shift-a" : () => {
+                    //console.log("Activate AI");
+                    //take the last 30 words
+                    const prompt = this.editor.getText().split(' ').slice(-30).join(' ');
+                    //console.log(prompt);
+                    complete(prompt);
+                    return true;
+                },
+            };
+        },
+    });
+
     const editor = useEditor({
         autofocus: true,
-        extensions: [StarterKit],
+        extensions: [StarterKit, customText],
         content: editorState,
         onUpdate: ({editor}) => {
             setEditorState(editor.getHTML());
         },
     });
+
+    const lastCompletion = React.useRef("");
+
+    React.useEffect(() => {
+        if(!completion || !editor) return;
+        const diff = completion.slice(lastCompletion.current.length);
+        lastCompletion.current = completion;
+        editor.commands.insertContent(diff);
+    }, [completion, editor]);
 
     const debouncedEditorState = useDebounce(editorState, 500);
     React.useEffect(() => {
